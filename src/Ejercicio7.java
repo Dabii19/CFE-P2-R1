a98f5d35b6c03fc37616f353e7ed927a98cc737a
Mateus Moraes <mateowalker9792@gmail.com>
2025-03-29 12:20:23 -0300

    Ejercicios del 1 al 7 completos

--- src/Ejercicio7.java
+++ src/Ejercicio7.java
@@ -1,2 +1,20 @@
+import java.util.Scanner;
+
 public class Ejercicio7 {
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+
+        System.out.println("Ingrese su nombre");
+        String nombre = leer.nextLine();
+
+        System.out.println(("Ingrese su edad"));
+        int edad = leer.nextInt();
+
+        leer.nextLine();
+        System.out.println("Ingrese la ciudad en la que vive");
+        String ciudad = leer.nextLine();
+
+        System.out.println("Hola, tu nombre es " + nombre + " tenés " + edad + " años y vivis en " + ciudad);
+
+    }
 }
